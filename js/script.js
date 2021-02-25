be44e16ad74f4d4fa91092915c1ec34e245e691a
shawn <shawn@nunoo.attlocal.net>
2021-02-24 17:29:40 -0800

    added js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//My name
+
+var myObj = { firstname: 'Ari', lastname: 'Agress' };
+console.log(myObj);
+
+/* My favorite food
+is below */
+
+var myArr = ['Burrito', 'Pizza', 'Pho', 'Sushi'];
+console.log(myArr);
